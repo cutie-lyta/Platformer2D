6c572cf83403950b11049023b490a8b056758ee0
elagmae <lea.pouthier@gmail.com>
2024-04-04 14:57:30 +0200

    création du vfx spawn

--- Assets/Scripts/Player/PlayerSpawn.cs
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -1,16 +1,25 @@
 using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField]
     private Material _material;
+    [SerializeField]
+    private VisualEffect _effectSpawn;
     private Material _default;
 
     private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        var rate = 4.0f;
+        _effectSpawn.playRate = rate;
+    }
     void Start()
     {
+        _effectSpawn.Play();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _default = _spriteRenderer.material;
         StartCoroutine(Spawn());
@@ -21,6 +30,7 @@ public class PlayerSpawn : MonoBehaviour
         _spriteRenderer.material = _material;
         DOTween.To(() => _spriteRenderer.material.GetFloat("_Intensity"), x => _spriteRenderer.material.SetFloat("_Intensity", x), 0.0f, 0.3f);
         yield return new WaitForSeconds(0.3f);
+        _effectSpawn.Stop();
         _spriteRenderer.material = _default;
     }
 
